4e5b5c9c9fe6f7972b7abc5221bf0a3a35ea293d
Adrian Utomo <adrianhakimutomo@gmail.com>
2024-05-27 21:13:52 +0700

    chore: 2 urls

--- tester.py
+++ tester.py
@@ -2,12 +2,14 @@ import requests
 import time
 import json
 
-def test_api_response_speed(url, payload):
+def test_api_response_speed(url1, url2, payload):
     response_times = []
     headers = {"Content-Type": "application/json"}
     for _ in range(5):
         start_time = time.time()
-        response = requests.post(url, data=json.dumps(payload), headers=headers)
+        preprocess_response = requests.post(url1, data=json.dumps(payload), headers=headers)
+        preprocessed_question = {"question": preprocess_response.json()["preprocessed_question"]}
+        response = requests.post(url2, data=json.dumps(preprocessed_question), headers=headers)
         end_time = time.time()
         response_time = end_time - start_time
         response_times.append(response_time)
@@ -18,16 +20,19 @@ def test_api_response_speed(url, payload):
 
 if __name__ == "__main__":
     print("Testing FastAPI application...")
-    fastapi_url = "http://localhost:8000/get_answer"
-    fastapi_payload = {"question": "I need help"}
-    test_api_response_speed(fastapi_url, fastapi_payload)
+    fastapi_url1 = "http://localhost:8000/preprocess_question"
+    fastapi_url2 = "http://localhost:8000/get_answer"
+    fastapi_payload = {"text": "I need help"}
+    test_api_response_speed(fastapi_url1, fastapi_url2, fastapi_payload)
 
     print("\nTesting Flask application...")
-    flask_url = "http://localhost:5000/get_answer"
-    flask_payload = {"question": "I need help"}
-    test_api_response_speed(flask_url, flask_payload)
+    flask_url1 = "http://localhost:5000/preprocess_question"
+    flask_url2 = "http://localhost:5000/get_answer"
+    flask_payload = {"text": "I need help"}
+    test_api_response_speed(flask_url1, flask_url2, flask_payload)
 
     print("\nTesting Django REST application...")
-    django_url = "http://localhost:8080/get_answer"
-    django_payload = {"question": "I need help"}
-    test_api_response_speed(django_url, django_payload)
+    django_url1 = "http://localhost:8080/preprocess_question"
+    django_url2 = "http://localhost:8080/get_answer"
+    django_payload = {"text": "I need help"}
+    test_api_response_speed(django_url1, django_url2, django_payload)
